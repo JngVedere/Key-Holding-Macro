d6f777141801ec4d76e4d1fa3a7812ddebf029d0
Lulzhub <88299137+JngVedere@users.noreply.github.com>
2023-03-06 22:52:04 +0900

    Key Holding Macro 0.1.0
    ver 0.1.0 - Released on 03-06-2023

---
+++ KeyHolding.py
@@ -0,0 +1,155 @@
+#
+#
+#       KeyHolding.py written by JngVedere
+#       Github : https://github.com/JngVedere
+#       version 0.1.0 - Released on 03-06-2023
+#
+#
+
+import win32gui
+import ctypes
+import os
+import threading
+from time import sleep
+from tkinter import messagebox
+
+# // Global variables
+key_pressing:bool = False
+target_key:int = 0
+hwnd:int = 0
+last_active_window = None
+
+# // ClasDD init
+try:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dd_dll = ctypes.windll.LoadLibrary(dir_path + '//DD94687.64.dll')
+    st = dd_dll.DD_btn(0) # classDD init
+except:
+    raise SystemExit("DD94687.64.dll Not Found")
+
+if st==1:
+    print('ClassDD Ready')
+else:
+    print('ERROR')
+    raise SystemExit("DD94687.64.dll Not Imported")
+
+# // Logics send key to window
+class KeyToWindow:
+
+    @staticmethod
+    def send_key_to_window(window_info, key:int, key_down = False):  
+        global hwnd, key_pressing,target_key, wnd_check_thread
+
+        # Check window_info is 'hwnd' or 'window name'
+        if isinstance(window_info, int): # if window_info is hwnd(int) value
+            hwnd = window_info
+        elif window_info.isdigit(): # if window_info is hwnd(str) value
+            hwnd = int(window_info)
+        else: # if window_info is window_name(str) value
+            hwnd = win32gui.FindWindow(None, window_info)
+        
+        if not KeyToWindow.is_valid_hwnd(hwnd): return
+
+        #Key pressing process
+        target_key = key
+        
+        if key_down == True:
+            if key_pressing == False:
+                key_pressing = True
+                dd_dll.DD_key(target_key,1) #DD_key(#_key, 1 for keydown/2 for keyUp)
+                print("keyDown", key_pressing)
+                return True
+            
+        elif key_down == False:
+            if key_pressing == True:
+                key_pressing = False
+                dd_dll.DD_key(target_key,2)
+                print("keyUp", key_pressing)
+                return False
+    
+    @staticmethod
+    def send_key(is_active):
+        KeyToWindow.send_key_to_window(hwnd, target_key,key_down=is_active)
+
+    @staticmethod
+    def is_valid_hwnd(hwnd) -> bool:
+        # ERROR case
+        if not win32gui.IsWindow(hwnd):
+            messagebox.showerror("ERROR","error code 2 : Window not found")
+            return False
+        
+        return True
+    
+    @staticmethod
+    def is_valid_window_info(window_info) -> bool:
+        temp_hwnd = ''
+
+        # Check window_info is 'hwnd' or 'window name'
+        if isinstance(window_info, int): # if window_info is hwnd(int) value
+            temp_hwnd = window_info
+        elif window_info.isdigit(): # if window_info is hwnd(str) value
+            temp_hwnd = int(window_info)
+        else: # if window_info is window_name(str) value
+            temp_hwnd = win32gui.FindWindow(None, window_info)
+        
+        return KeyToWindow.is_valid_hwnd(temp_hwnd)
+    
+# // Operations for checking window switched
+class WindowActiveChecker:
+    global hwnd
+
+    @staticmethod
+    def is_target_active(active_window):      
+        KeyToWindow.send_key(hwnd == active_window)
+
+    @staticmethod
+    def detect_window_switch():
+        global last_active_window
+
+        # Loop indefinitely
+        # Get the active window handle
+        active_window = win32gui.GetForegroundWindow()
+
+        # If the active window is different from the last active window
+        if active_window != last_active_window:
+            # Check active window is target window
+            WindowActiveChecker.is_target_active(active_window)
+
+            # # Get the window text
+            # window_text = win32gui.GetWindowText(active_window)
+            
+            # # Print the window text and window class
+            # print("Window switched: %s (%s)" % (window_text, win32gui.GetClassName(active_window)))
+
+            # Set the last active window to the current active window
+            last_active_window = active_window 
+
+# // Logics Threading
+class MyThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.pause_event = threading.Event()
+        self.pause_event.clear()
+        self.daemon = True
+
+    def run(self):
+        # while not self.pause_event.is_set():
+        while True:
+            self.pause_event.wait()
+            WindowActiveChecker.detect_window_switch()
+            # Wait for a short time before checking again
+            sleep(0.1)
+
+    def pause(self):
+        self.pause_event.clear()
+
+    def resume(self):
+        self.pause_event.set()
+
+def getKeyPressing() -> bool:
+    global key_pressing
+    return key_pressing
+if __name__ != '__main__':
+    wnd_check_thread = MyThread()
+    sleep(0.1)
+    wnd_check_thread.start()
---
+++ main.py
@@ -0,0 +1,195 @@
+#
+#
+#       Key-Holding-Macro made by JngVedere
+#       Github : https://github.com/JngVedere
+#       version 0.1.0 - Released on 03-06-2023
+#
+#
+
+
+
+from tkinter import messagebox, ttk
+from tendo import singleton
+try:
+    singleton.SingleInstance()
+except SystemExit as e:
+    messagebox.showerror("ERROR", e)
+
+import tkinter as tk
+import pywinauto, XK_TO_DD
+import threading
+from time import sleep
+
+try:
+    from KeyHolding import KeyToWindow, getKeyPressing
+    from KeyHolding import wnd_check_thread
+
+except SystemExit as e:
+    messagebox.showerror("ERROR",e)
+    exit(0)
+
+# // Global variables
+root:object = tk.Tk()
+app:object
+app_title:str = "Key Holding Macro"
+app_size:tuple = (250,125)
+window_info:str = ""
+simplified:bool = True
+
+# // GUI
+class MainApp:
+    
+    def __init__(self, master):
+        global simplified, key_input_label
+
+        # Basic Variables
+        self.master = master
+        self.key_pressed = False
+        self.key_to_send = -1
+
+        #Frame
+        self.master.title(app_title)
+        self.X = int(self.master.winfo_screenwidth()/2 - app_size[0]/2)
+        self.Y = int(self.master.winfo_screenheight()/2 - app_size[1]/2)
+        self.master.wm_geometry(f"{app_size[0]}x{app_size[1]}+{self.X}+{self.Y}")
+        self.master.minsize(120, 100)
+        self.master.bind("<Key>", self.key_press)
+
+        self.window_combobox = ttk.Combobox(self.master, width = 40, postcommand = lambda:self.update_cb_list(simplified), state='readonly')
+        self.window_combobox.set("Pick a Window")
+        self.window_combobox.pack(fill="x",padx=3,pady=1)
+        self.window_combobox.bind("<<ComboboxSelected>>",self.window_selected)
+
+        self.check_var = tk.BooleanVar(value=True)
+        self.simplified_checkbutton = tk.Checkbutton(self.master, text='Simplified Window', variable=self.check_var, onvalue=True, offvalue=False)
+        self.simplified_checkbutton.bind("<ButtonRelease-1>",self.on_check_button_click)
+        self.simplified_checkbutton.pack(pady=3,side="top")
+        print(self.check_var.get())
+
+        self.show_key = tk.Label(self.master, text="<Press any key to hold>", bg='gray19', fg='snow')
+        self.show_key.pack()
+
+        self.send_button = tk.Button(self.master, text="Hold Key", command=self.button_pressed)
+        self.send_button.pack(pady=3)
+
+        self.ro_textbox = ttk.Label(master, text=' ')
+        self.ro_textbox.pack()
+
+    def update_cb_list(self, simplified):
+        print("updt cb list", simplified)
+        if simplified == True: # Find window by window name
+            self.temp_list = pywinauto.Desktop(backend='uia').windows(title_re ='.')
+            self.values_list = [w.window_text() for w in self.temp_list]
+
+        else: # Find window by hwnd
+            self.values_list = []
+            self.hwnd_list = []
+            procs = pywinauto.findwindows.find_elements()
+            for proc in procs:
+                self.hwnd_list.append(proc.handle)
+                self.values_list.append((proc.name,proc.class_name))   
+
+        self.window_combobox['value'] = self.values_list
+    
+    def on_check_button_click(self, event):
+        def update_check_var(): #To Avoid Firing two functional works
+            print("Button Clicked")
+            global simplified
+            simplified = self.check_var.get()
+            self.window_combobox.set("Pick a Window")
+            print(self.check_var.get())
+
+        self.master.after(30, update_check_var)
+
+    def window_selected(self, event):
+        global window_info
+        if simplified == True:
+            window_info = self.window_combobox.get()
+        elif simplified == False:
+            window_info = self.hwnd_list[self.window_combobox.current()]
+
+    def key_press(self, event):
+        if self.key_pressed == False:
+            self.show_key.config(text=event.keysym)
+            self.key_to_send = XK_TO_DD.XK_TO_DD[str(event.keycode)]
+            print(repr(event.char), repr(event.keysym), repr(event.keycode), repr(event.keysym_num))
+
+    def button_pressed(self):
+        global window_info
+        
+        if self.window_combobox.current() == -1:
+            messagebox.showerror("ERROR", "Window isn't selected")  
+            return
+        
+        elif self.key_to_send == -1:
+            messagebox.showerror("ERROR", "Key isn't selected")
+            return
+
+        if not KeyToWindow.is_valid_window_info(window_info): return
+        print(window_info)
+
+        if not self.key_pressed:
+            KeyToWindow.send_key_to_window(window_info, self.key_to_send, key_down=True)
+            self.key_pressed = True
+            wnd_check_thread.resume()
+            key_check_thread.resume()
+            self.window_combobox.config(state='disabled')
+            self.simplified_checkbutton.config(state='disabled')
+            self.ro_textbox.config(text='')
+            self.send_button.config(text="Release Key")
+            
+                
+        else:
+            KeyToWindow.send_key_to_window(window_info, self.key_to_send, key_down=False)
+            self.key_pressed = False
+            wnd_check_thread.pause()
+            key_check_thread.pause()
+            self.window_combobox.config(state='normal')
+            self.simplified_checkbutton.config(state='normal')
+            self.ro_textbox.config(text='')
+            self.send_button.config(text="Hold Key")
+    
+    def SafeQuit(self, master:object = root) -> None:
+        if messagebox.askokcancel(f"{app_title} Quit", f"Are you sure that you want to quit {app_title}?"):
+            if getKeyPressing() == True:
+                KeyToWindow.send_key_to_window(window_info, self.key_to_send, key_down=False)
+                print("Events Listening is stopped!")
+            master.destroy()
+
+    def is_input_activating(self):
+        print("is_intput_activating executed")
+        if getKeyPressing() == True:
+            print("is~~~ true")
+            self.ro_textbox.config(text='Activating')
+        else:
+            print("is~~~ false")
+            self.ro_textbox.config(text='Not Activating')
+
+#// Logics Threading
+class MyThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.pause_event = threading.Event()
+        self.pause_event.clear()
+        self.daemon = True
+
+    def run(self):
+        # while not self.pause_event.is_set():
+        while True:
+            self.pause_event.wait()
+            app.is_input_activating()
+            # Wait for a short time before checking again
+            sleep(0.1)
+
+    def pause(self):
+        self.pause_event.clear()
+
+    def resume(self):
+        self.pause_event.set()
+
+if __name__ == "__main__":
+    app = MainApp(root)
+    key_check_thread = MyThread()
+    key_check_thread.start()
+    root.protocol("WM_DELETE_WINDOW", app.SafeQuit)
+    root.mainloop()
